337899d6caeee169b6df24369af7f3d3189a1fc8
Deven Kalra <deven@Macbook.local>
2023-11-30 05:43:13 +0530

    Added Files Through Upload

---
+++ src/main/java/com/cleo/domain/User.java
@@ -0,0 +1,19 @@
+package com.cleo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+
+@Entity
+@Data
+@Getter
+public class User {
+    @Id @GeneratedValue
+    private Long id;
+
+
+    private String userName;
+
+}
---
+++ src/main/java/com/cleo/utils/Email.java
@@ -0,0 +1,4 @@
+package com.cleo.utils;
+
+public interface Email {
+}
