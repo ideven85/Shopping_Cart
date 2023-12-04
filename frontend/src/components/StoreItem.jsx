d0037fab0dcb72545a8f1a0b81d86f03d293c3cd
Deven Kalra <deven@Macbook.local>
2023-12-05 01:09:01 +0530

    Added Files Through Upload

--- frontend/src/components/StoreItem.jsx
+++ frontend/src/components/StoreItem.jsx
@@ -1,32 +1,69 @@
-import {Button, Card} from "react-bootstrap";
-import {currencyFormatter} from "../utitlties/FormatCurency.js";
-export function StoreItem({id,name,price,imgUrl}){
-    const quantity=1;
-    return <Card className="h-100">
-        <Card.Img variant="top" src={imgUrl} height="200px" style={{objectFit: "cover"}} />
-        <Card.Body className="d-flex flex-column">
+import { Button, Card } from "react-bootstrap"
+import { useShoppingCart } from "../context/ShoppingCartContext"
+import { currencyFormatter } from "../utitlties/FormatCurency"
 
-            <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
-              <span className="fs-2">  {name}</span>
-                <span className="ms-2 text-muted">{currencyFormatter(price)} </span>
+// type StoreItemProps = {
+//   id: number
+//   name: string
+//   price: number
+//   imgUrl: string
+// }
 
-            </Card.Title>
-            <div className="mt-auto">{quantity=== 0?
-                (<Button
-                className="w-100">+ Add To Cart</Button>
-                ):(<div
-                className="d-flex align-items-center flex-column"
-                style={{ gap: ".5rem" }}>
-                <div
+// eslint-disable-next-line react/prop-types
+export function StoreItem({ id, name, price, imgUrl }) {
+  const {
+    getItemQuantity,
+    increaseItemQuantity,
+    decreaseItemQuantity,
+    removeFromCart
+  } = useShoppingCart()
+  const quantity = getItemQuantity(id)
+
+  return (
+    <Card className="h-100">
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        height="200px"
+        style={{ objectFit: "cover" }}
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{currencyFormatter(price)}</span>
+
+        </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100" onClick={() => increaseItemQuantity(id)}>
+            + Add To Cart
+            </Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5rem" }}
+            >
+              <div
                 className="d-flex align-items-center justify-content-center"
-                style={{
-                gap: ".5rem" }}>
-                Hi
+                style={{ gap: ".5rem" }}
+              >
+                <Button onClick={() => decreaseItemQuantity(id)}>-</Button>
+                <div>
+                  <span className="fs-3">{quantity}</span> in cart
                 </div>
-                Bye
-                </div>
-                )}
+                <Button onClick={() => increaseItemQuantity(id)}>+</Button>
+              </div>
+              <Button
+                onClick={() => removeFromCart(id)}
+                variant="danger"
+                size="sm"
+              >
+                Remove
+              </Button>
             </div>
-        </Card.Body>
+          )}
+        </div>
+      </Card.Body>
     </Card>
+  )
 }
--- frontend/src/pages/Store.jsx
+++ frontend/src/pages/Store.jsx
@@ -6,11 +6,12 @@ export function Store(){
     return <>
         <h1>Store</h1>
 
-        <Row md={2} xs={1} lg={3} className="g-3">
-            {storeItems.map(item=>(
-                <Col key={item.id}><StoreItem {...item} /></Col>
-
-            ))}
-        </Row>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map(item => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
     </>
 }
